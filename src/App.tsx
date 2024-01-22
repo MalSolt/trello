e56571ae5c486cacd51b7752fbf7dbe26d458105
Malik Soltukiev <freelance99@bk.ru>
2024-01-22 13:33:27 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useDraggable, useDroppable } from '@dnd-kit/core'
+
+import { DndContext } from '@dnd-kit/core'
+import { useState } from 'react'
 
 function App() {
+  const [isDropped, setIsDropped] = useState(false)
+  const draggableMarkup = (
+    <Draggable>
+      <div className='drag_item'>Drag me</div>
+    </Draggable>
+  )
+
+  function handleDragEnd(event: any) {
+    if (event.over && event.over.id === 'droppable') {
+      setIsDropped(true)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <DndContext onDragEnd={handleDragEnd}>
+      <div className='section'>
+        <div className='draggable'>{isDropped ? null : draggableMarkup}</div>
+        <div className='draggable'>
+          <Droppable>{isDropped ? draggableMarkup : 'Drop here'}</Droppable>
+        </div>
+      </div>
+    </DndContext>
+  )
 }
 
-export default App;
+export default App
+
+const Draggable = (props: any) => {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: 'draggable',
+  })
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined
+
+  return (
+    <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      {props.children}
+    </button>
+  )
+}
+
+const Droppable = (props: any) => {
+  const { isOver, setNodeRef } = useDroppable({
+    id: 'droppable',
+  })
+  const style = {
+    color: isOver ? 'blue' : undefined,
+  }
+
+  return (
+    <div ref={setNodeRef} style={style}>
+      {props.children}
+    </div>
+  )
+}
